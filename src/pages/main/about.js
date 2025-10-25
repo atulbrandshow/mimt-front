f3db46bc250ae8d4f2cc4b6d7c9900df63bb95b9
atul-xt <atul.xtt@gmail.com>
2025-10-25 12:05:11 +0530

    fixed

--- src/pages/main/about.js
+++ src/pages/main/about.js
@@ -3,7 +3,7 @@ import React from 'react'
 const about = ({ data }) => {
     return (
         <div className='h-screen w-full flex justify-center items-center'>
-            <h1 className='text-5xl font-novaBold'>{data.name}</h1>
+            <h1 className='text-5xl font-novaBold'>{data?.name}</h1>
         </div>
     )
 }
