fe1e1ae4f8fddb3e522e1f7e512c97bf38e7c2b9
atul-xt <atul.xtt@gmail.com>
2025-10-31 15:56:27 +0530

    fixed

--- src/pages/pagesComp/HomePage.js
+++ src/pages/pagesComp/HomePage.js
@@ -2,6 +2,7 @@
 
 import AboutSection from "@/component/AboutSection";
 import CampusSlider from "@/component/CampusSlider";
+import { EventSection } from "@/component/EventSection";
 import ExploreCourses from "@/component/ExploreCourses";
 import FooterCard from "@/component/FooterCard";
 import HomeLogoSlider from "@/component/HomeLogoSlider";
@@ -10,12 +11,16 @@ import InnovationShowcase from "@/component/InnovationShowcase";
 import MainSection from "@/component/MainSection";
 import MilestonesSection from "@/component/MilestonesSection";
 import NewsEvents from "@/component/NewsEvents";
+import NewsSection from "@/component/NewsSection";
 import NotificationSlider from "@/component/NotificationSlider";
 import PlacementHighlights from "@/component/PlacementHighlights";
 import PlacementOverview from "@/component/PlacementOverview";
+import { ProgramSection } from "@/component/ProgramSection";
+import { RecruiterTestimonial } from "@/component/RecruiterTestimonial";
 import ResearchEnvironment from "@/component/ResearchEnvironment";
 import SlickSlider from "@/component/SlickSlider";
 import StudentReviews from "@/component/StudentReviews";
+import { StudentTestimonial } from "@/component/StudentTestimonial";
 
 
 export default function HomePage({ data }) {
@@ -27,14 +32,19 @@ export default function HomePage({ data }) {
     <>
       {data && <MainSection data={data} />}
       <AboutSection />
-      {data && <HomeTopCard data={data} />}
+      {data && <MilestonesSection data={data} />}
+      <EventSection />
+      <ProgramSection />
+      {/* {data && <HomeTopCard data={data} />} */}
       {data && <ExploreCourses data={data} />}
       {data && <SlickSlider data={data} />}
       {data && <NotificationSlider data={data} />}
-      {data && <MilestonesSection data={data} />}
-      <InnovationShowcase />
+      <NewsSection />
+      <StudentTestimonial />
       {/* {data && <PlacementHighlights data={data} />} */}
       <PlacementOverview />
+      <RecruiterTestimonial />
+      <InnovationShowcase />
       {/* {data && <HomeLogoSlider data={data} />} */}
       {/* {data && <CampusSlider data={data} />} */}
       {/* {data && <StudentReviews data={data} />} */}
