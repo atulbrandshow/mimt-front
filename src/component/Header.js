3b904452803c914f2d3f5a400f19abd69fade2f7
yatishprajapatbrandshow <yatish.prajapat@brandshow.in>
2025-11-03 14:46:56 +0530

    New Push

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Breadcrumb from './Breadcrumb'
 
-const Header = ({ BreadCrumb }) => {
+const Header = ({ BreadCrumb, data }) => {
     return (
         <div
             className="relative w-full h-[50vh] bg-cover bg-center"
@@ -12,7 +12,7 @@ const Header = ({ BreadCrumb }) => {
             <div className="absolute inset-0 bg-gradient-to-r from-black via-black/50 to-transparent"></div>
             <div className="absolute inset-0 flex items-center">
                 <div className="max-w-[1600px] mx-auto px-3 sm:px-6 h-full w-full flex flex-col justify-center gap-5">
-                    <h1 className="text-5xl font-novaReg text-secondary">Overview</h1>
+                    <h1 className="text-5xl font-novaReg text-secondary capitalize">{data?.name || "Overview"}</h1>
                     <div>
                         <Breadcrumb data={BreadCrumb} color="white" />
                     </div>
