aa2c381551b781c3ae44c4b53639084ffda9dd59
yatishprajapatbrandshow <yatish.prajapat@brandshow.in>
2025-11-04 14:18:02 +0530

    new push

--- src/component/ui/animated-tooltip.js
+++ src/component/ui/animated-tooltip.js
@@ -8,10 +8,12 @@ import {
     useMotionValue,
     useSpring,
 } from "motion/react";
+import { useRouter } from "next/router";
 
 export const AnimatedTooltip = ({
     items
 }) => {
+    const router = useRouter()
     const [hoveredIndex, setHoveredIndex] = useState(null);
     const springConfig = { stiffness: 100, damping: 15 };
     const x = useMotionValue(0);
@@ -35,7 +37,7 @@ export const AnimatedTooltip = ({
         <>
             {items.map((item, idx) => (
                 <div
-                    className="group relative -mr-4"
+                    className="group relative mb-2  bg-white rounded-full cursor-pointer"
                     key={item.name}
                     onMouseEnter={() => setHoveredIndex(item.id)}
                     onMouseLeave={() => setHoveredIndex(null)}>
@@ -59,7 +61,8 @@ export const AnimatedTooltip = ({
                                     rotate: rotate,
                                     whiteSpace: "nowrap",
                                 }}
-                                className="absolute -top-16 left-1/2 z-50 flex -translate-x-1/2 flex-col items-center justify-center rounded-md bg-black px-4 py-2 text-xs shadow-xl">
+                                onClick={() => router.push(item?.link)}
+                                className="absolute cursor-pointer -top-10 left-1/2 z-50 flex -translate-x-1/2 flex-col items-center justify-center rounded-md bg-primary/50  border-[1px] border-white px-4 py-2 text-xs shadow-xl">
                                 <div
                                     className="absolute inset-x-10 -bottom-px z-30 h-px w-[20%] bg-gradient-to-r from-transparent via-emerald-500 to-transparent" />
                                 <div
@@ -67,7 +70,6 @@ export const AnimatedTooltip = ({
                                 <div className="relative z-30 text-base font-bold text-white">
                                     {item.name}
                                 </div>
-                                <div className="text-xs text-white">{item.designation}</div>
                             </motion.div>
                         )}
                     </AnimatePresence>
@@ -77,7 +79,7 @@ export const AnimatedTooltip = ({
                         width={100}
                         src={item.image}
                         alt={item.name}
-                        className="relative !m-0 h-14 w-14 rounded-full border-2 border-white object-cover object-top !p-0 transition duration-500 group-hover:z-30 group-hover:scale-105" />
+                        className="relative  !m-0 h-9 w-9 rounded-full border-2 border-white object-contain object-top p-1 transition duration-500 group-hover:z-30 group-hover:scale-105" />
                 </div>
             ))}
         </>
