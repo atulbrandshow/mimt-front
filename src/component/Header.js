51ba1c85ff2a771f2ec31bcf1d67fe41bf8f4c6d
atul-xt <atul.xtt@gmail.com>
2025-11-05 13:49:05 +0530

    fixed

--- src/component/Header.js
+++ src/component/Header.js
@@ -14,7 +14,7 @@ const Header = ({ BreadCrumb, data }) => {
                 <div className="max-w-[1500px] mx-auto px-3 sm:px-10 h-full w-full flex flex-col justify-center gap-5">
                     <h1 className="text-5xl font-novaReg text-secondary capitalize">{data?.name || "Overview"}</h1>
                     <div>
-                        <Breadcrumb data={BreadCrumb} color="white" />
+                        {BreadCrumb && <Breadcrumb data={BreadCrumb} color="white" />}
                     </div>
                 </div>
             </div>
