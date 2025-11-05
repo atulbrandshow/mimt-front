8ed934a6a4e3662dbdae5ebdd6bdedec118f6e30
atul-xt <atul.xtt@gmail.com>
2025-11-05 15:45:05 +0530

    fixed

--- src/pages/main/mission-vision.js
+++ src/pages/main/mission-vision.js
@@ -10,9 +10,12 @@ const d = data?.pageData;
   return (
     <div className="bg-white">
       <Header BreadCrumb={data?.breadCrumb} data={data} />
-      <section className="w-full max-w-[1600px] mx-auto grid grid-cols-12 py-20 max-sm:py-2 gap-10 px-3 sm:px-6 max-sm:gap-0">
-        <div className="col-span-9 max-xl:col-span-8 max-lg:col-span-12">
-          <div className="w-full flex justify-center">
+      <div className="bg-primary">
+        <div className="bg-white h-20 rounded-bl-3xl" />
+      </div>
+      <section className="w-full grid grid-cols-12 gap-10 max-sm:gap-0">
+        <div className="bg-primary rounded-r-[50px] col-span-9 max-xl:col-span-8 max-lg:col-span-12">
+          <div className="max-w-6xl ml-auto py-16 px-5">
             <div className="max-w-5xl w-full space-y-14">
               <div className="relative bg-white p-10 rounded-2xl shadow-lg border border-gray-200">
                 {/* Yellow Highlight Bar */}
