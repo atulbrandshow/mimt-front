f78ab8e7bf7f912fb2561af118d2bdd021eb07c6
atul-xt <atul.xtt@gmail.com>
2025-11-05 16:40:38 +0530

    fixed

--- src/pages/main/advantagemangalmay.js
+++ src/pages/main/advantagemangalmay.js
@@ -20,30 +20,22 @@ export default function Page({ data }) {
 
   return (
     <div className="bg-white">
-      {/* Header */}
       <Header BreadCrumb={data?.breadCrumb} data={data} />
-
-      <section className="w-full max-w-[1600px] mx-auto grid grid-cols-12 py-20 max-sm:py-2 gap-10 px-3 sm:px-6 max-sm:gap-0">
-
-        {/* LEFT CONTENT */}
-        <div className="col-span-9 max-xl:col-span-8 max-lg:col-span-12">
-
-          {/* Title */}
-      
-
-          {/* ✅ Render HTML content safely */}
-          <div
-            className={`${descriptionCss} mt-4 prose prose-gray max-w-none`}
-            dangerouslySetInnerHTML={{ __html: htmlContent }}
-          />
-
+      <div className="bg-primary">
+        <div className="bg-white h-20 rounded-bl-3xl" />
+      </div>
+      <section className="w-full grid grid-cols-12 gap-10 max-sm:gap-0">
+        <div className="bg-primary rounded-r-[50px] col-span-9 max-xl:col-span-8 max-lg:col-span-12">
+          <div className="max-w-6xl ml-auto py-16 px-5">
+            <div
+              className={`mt-4 prose prose-gray max-w-none prose-li:text-white font-novaReg`}
+              dangerouslySetInnerHTML={{ __html: htmlContent }}
+            />
+          </div>
         </div>
-
-        {/* RIGHT SIDEBAR */}
         <div className="col-span-3 max-xl:col-span-4 max-lg:col-span-12">
           <SideBar title={"About Us"} LinkList={SideBarLink} />
         </div>
-
       </section>
     </div>
   );
