32e9dd8b726193dab2b23511053484fa6d41e494
atul-xt <atul.xtt@gmail.com>
2025-11-07 13:11:30 +0530

    fixed

--- src/pages/main/hostel.js
+++ src/pages/main/hostel.js
@@ -19,27 +19,26 @@ export default function Page({ data }) {
   return (
     <div className="bg-white">
       <Header BreadCrumb={data?.breadCrumb} data={data} />
+      <div className="bg-primary">
+        <div className="bg-white h-20 rounded-bl-3xl" />
+      </div>
+      <section className="w-full grid grid-cols-12 gap-10 max-sm:gap-0">
+        <div className="bg-primary rounded-r-[50px] col-span-9 max-xl:col-span-8 max-lg:col-span-12">
+          <div className="max-w-[1200px] ml-auto py-16 px-16">
+            <div
+              dangerouslySetInnerHTML={{ __html: data?.pageData?.HostelDescription }}
+              className={`${descriptionCss} bg-gray-50 p-4 rounded-xl`}
+            />
 
-      <section className="w-full max-w-[1600px] mx-auto grid grid-cols-12 py-20 max-sm:py-2 gap-10 px-3 sm:px-6 max-sm:gap-0">
-
-        {/* ✅ LEFT SECTION */}
-        <div className="col-span-9 max-xl:col-span-8 max-lg:col-span-12">
-
-          {/* ✅ HOSTEL DESCRIPTION */}
-          <div
-            dangerouslySetInnerHTML={{ __html: data?.pageData?.HostelDescription }}
-            className={descriptionCss}
-          />
-
-          {/* ✅ HOSTEL GALLERY */}
-          <section className="py-12 bg-white">
-            <div className="max-w-7xl mx-auto">
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                <MIMTGallery data={data?.pageData?.HostelGallery} />
+            {/* ✅ HOSTEL GALLERY */}
+            <section className="py-6">
+              <div className="max-w-7xl mx-auto">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                  <MIMTGallery data={data?.pageData?.HostelGallery} />
+                </div>
               </div>
-            </div>
-          </section>
-
+            </section>
+          </div>
         </div>
 
         {/* ✅ RIGHT SIDEBAR */}
