3de74ef2c1e4705703c03ec1327059831e65b691
atul-xt <atul.xtt@gmail.com>
2025-11-07 10:09:26 +0530

    fixed

--- src/pages/main/computer-center.js
+++ src/pages/main/computer-center.js
@@ -19,35 +19,28 @@ export default function Page({ data }) {
   return (
     <div className="bg-white">
       <Header BreadCrumb={data?.breadCrumb} data={data} />
-
-      <section className="w-full max-w-[1600px] mx-auto grid grid-cols-12 py-20 max-sm:py-6 gap-10 px-3 sm:px-6">
-
-        {/* ✅ LEFT CONTENT */}
-        <div className="col-span-9 max-xl:col-span-8 max-lg:col-span-12">
-
-          {/* ✅ PAGE TITLE */}
-          <h2 className="text-3xl font-bold text-black mb-6 border-l-8 border-[#fdd023] pl-4">
-            Computer Center
-          </h2>
-
-          {/* ✅ DESCRIPTION */}
-          <div
-            dangerouslySetInnerHTML={{ __html: data?.pageData?.ComputerCenterDescription }}
-            className={`${descriptionCss} bg-gray-50 p-6 rounded-xl shadow-sm leading-relaxed text-[17px]`}
-          />
-
-          {/* ✅ GALLERY */}
-          <section className="py-12 bg-white">
-            <div className="max-w-7xl mx-auto">
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
-                <MIMTGallery data={data?.pageData?.ComputerCenterGallery} />
+      <div className="bg-primary">
+        <div className="bg-white h-20 rounded-bl-3xl" />
+      </div>
+      <section className="w-full grid grid-cols-12 gap-10 max-sm:gap-0">
+        <div className="bg-primary rounded-r-[50px] col-span-9 max-xl:col-span-8 max-lg:col-span-12">
+          <div className="max-w-[1200px] ml-auto py-16 px-16">
+            <h2 className="text-3xl font-novaBold text-white mb-6 border-l-8 border-secondary pl-4">
+              Computer Center
+            </h2>
+            <div
+              dangerouslySetInnerHTML={{ __html: data?.pageData?.ComputerCenterDescription }}
+              className={`${descriptionCss} bg-gray-50 p-6 rounded-xl font-novaReg shadow-sm leading-relaxed text-[17px]`}
+            />
+            <section className="py-12">
+              <div className="max-w-7xl mx-auto">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
+                  <MIMTGallery data={data?.pageData?.ComputerCenterGallery} />
+                </div>
               </div>
-            </div>
-          </section>
-
+            </section>
+          </div>
         </div>
-
-        {/* ✅ RIGHT SIDEBAR */}
         <div className="col-span-3 max-xl:col-span-4 max-lg:col-span-12">
           <SideBar title={"About Us"} LinkList={SideBarLink} />
         </div>
