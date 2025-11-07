6b6a5e91d6ee112f16ef3f011786a0a069203101
atul-xt <atul.xtt@gmail.com>
2025-11-07 12:06:54 +0530

    fixed

--- src/pages/main/seminar-halls.js
+++ src/pages/main/seminar-halls.js
@@ -20,17 +20,22 @@ export default function Page({ data }) {
   return (
     <div className="bg-white">
       <Header BreadCrumb={data?.breadCrumb} data={data} />
-      <section className="w-full max-w-[1600px] mx-auto grid grid-cols-12 py-20 max-sm:py-2 gap-10 px-3 sm:px-6 max-sm:gap-0">
-        <div className="col-span-9 max-xl:col-span-8 max-lg:col-span-12">
-          <div dangerouslySetInnerHTML={{ __html: data?.pageData?.SeminarHallDesc }} className={descriptionCss}/>
-          <section className="py-12 bg-white">
-            <div className="max-w-7xl mx-auto ">
-              {/* Gallery Grid */}
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                <MIMTGallery data={data?.pageData?.SeminarHallGallery} />
+      <div className="bg-primary">
+        <div className="bg-white h-20 rounded-bl-3xl" />
+      </div>
+      <section className="w-full grid grid-cols-12 gap-10 max-sm:gap-0">
+        <div className="bg-primary rounded-r-[50px] col-span-9 max-xl:col-span-8 max-lg:col-span-12">
+          <div className="max-w-[1200px] ml-auto py-16 px-16">
+            <div dangerouslySetInnerHTML={{ __html: data?.pageData?.SeminarHallDesc }} />
+            <section className="py-6">
+              <div className="max-w-7xl mx-auto ">
+                {/* Gallery Grid */}
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                  <MIMTGallery data={data?.pageData?.SeminarHallGallery} />
+                </div>
               </div>
-            </div>
-          </section>
+            </section>
+          </div>
         </div>
         <div className="col-span-3 max-xl:col-span-4 max-lg:col-span-12">
           <SideBar title={"About Us"} LinkList={SideBarLink} />
