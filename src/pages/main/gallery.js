167e94de0570e6ce8a8acd05e9df90d9f9c75362
atul-xt <atul.xtt@gmail.com>
2025-11-07 13:14:27 +0530

    fixed

--- src/pages/main/gallery.js
+++ src/pages/main/gallery.js
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "@/component/Header";
 import SideBar from "@/component/SideBar";
 import MIMTGallery from "@/component/MIMTGallery";
-import { descriptionCss } from "@/configs/css.config";
 
 export default function Page({ data }) {
   const SideBarLink = [
@@ -19,29 +18,25 @@ export default function Page({ data }) {
   return (
     <div className="bg-white">
       <Header BreadCrumb={data?.breadCrumb} data={data} />
-
-      <section className="w-full max-w-[1600px] mx-auto grid grid-cols-12 py-20 max-sm:py-4 gap-10 px-3 sm:px-6">
-
-        {/* ✅ LEFT SECTION */}
-        <div className="col-span-9 max-xl:col-span-8 max-lg:col-span-12">
-
-          {/* ✅ MIMT GALLERY COMPONENT */}
-          <section className="py-12 bg-white">
-            <div className="max-w-7xl mx-auto">
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                <MIMTGallery data={galleryImages} />
+      <div className="bg-primary">
+        <div className="bg-white h-20 rounded-bl-3xl" />
+      </div>
+      <section className="w-full grid grid-cols-12 gap-10 max-sm:gap-0">
+        <div className="bg-primary rounded-r-[50px] col-span-9 max-xl:col-span-8 max-lg:col-span-12">
+          <div className="max-w-[1200px] ml-auto py-16 px-16">
+            <section className="py-6">
+              <div className="max-w-7xl mx-auto">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                  <MIMTGallery data={galleryImages} />
+                </div>
               </div>
-            </div>
-          </section>
-
+            </section>
+          </div>
         </div>
-
-        {/* ✅ RIGHT SIDEBAR */}
         <div className="col-span-3 max-xl:col-span-4 max-lg:col-span-12">
           <SideBar title={"About Us"} LinkList={SideBarLink} />
         </div>
-
       </section>
-    </div>
+    </div >
   );
 }
